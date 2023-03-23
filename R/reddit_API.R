d5aae8ff9ccf9c23f0d6343d92c564fca716933c
Saron M Demeke <demek004@umn.edu>
2023-03-23 00:19:09 -0500

    first pass at part 3 complete. deleted temporary downloads. figure out how to save local image next

--- R/reddit_API.R
+++ R/reddit_API.R
@@ -8,15 +8,15 @@ library(tidyverse)
 #Data Import and Cleaning
 
 #first line download.file() saves local image of the rstats/.json
-download.file("https://www.reddit.com/r/rstats/.json","../data/rawredditjson.html")
+#download.file("https://www.reddit.com/r/rstats/.json","../data/raw_reddit_json.html")
 #fromJSON parson raw information as nested list objects, flatten argument collapses nested data frames
-rstats_list <- fromJSON("../data/rawredditjson.html", flatten = TRUE) 
+rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE) 
 #the df we want is stored in [["children"]] list element of rstats_list so we extract it
 rstats_original_tbl <- rstats_list$data$children
 #next we create new df with just the desired variables and rename the original JSON names which were collapsed i.e., data.selftext
-rstats_tbl <- rstats_original_tbl %>% 
+rstats_api_tbl <- rstats_original_tbl %>% 
    select(
-    post=data.selftext, #do we want the body text or the titles? some posts don't have inner text
+    post=data.title, #do we want the body text or the titles? some posts don't have inner text, taking title for now
     upvotes=data.ups,
     comments=data.num_comments
   ) 
@@ -24,15 +24,17 @@ rstats_tbl <- rstats_original_tbl %>%
 #Visualization
 
 #ggplot and geom_point plot scatterplot where x=number of upvotes and y=number of comments
-rstats_tbl %>% 
+rstats_api_tbl %>% 
   ggplot(aes(upvotes,comments)) +
   geom_point()
 
 #Analysis
 
 #cor.test() runs significance test on correlation between upvotes and comments
-cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
-#0.7857551 
+api_cortest <- cor.test(rstats_api_tbl$upvotes, rstats_api_tbl$comments)
+#0.7803738  
+   
 
 #Publication
-
+paste0("The correlation between upvotes and comments was r", "(", api_cortest$parameter[[1]] ,") = ",
+       round(api_cortest$estimate,2), ", p = ",  round(api_cortest$p.value,2))
--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -1,13 +1,60 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-
 library(rvest)
+library(tidyverse)
+library(xml2)
 
 
-# get_html <- read_html("https://www.reddit.com/r/rstats/")
 
-saveRDS(get_html,"../data/scrapedRedditstats.RDS") #saved response from 03/22 5:16pm
+#Data Import and Cleaning
+#download.file("https://old.reddit.com/r/rstats/","../data/raw_oldreddit.html")
+rstats_html <- read_html("https://old.reddit.com/r/rstats/")
 
-reddit_scrpd_response <- readRDS("../data/scrapedRedditstats.RDS")
+#using SelectorGadget, I identified the CSS selector to select all post titles (excluded some unnecessary text underneath some post titles)
+post <- rstats_html %>% 
+  html_elements(css='.title.may-blank') %>% 
+  html_text()
 
+#using Developer Tools/Inspect, I found the class name for each upvote and the text extracted is the number of upvotes
+#I found it easier to construct the xpath myself
+#coerced to numeric
+upvotes <- rstats_html %>% 
+  html_elements(xpath='//div[@class = "score unvoted"]') %>% 
+  html_text() %>% 
+  as.numeric() %>% 
+  replace_na(0)
+
+#used SelectorGadget to identify selector for every 'X comments' on page
+#before coercing to numeric, I remove all characters using str_remove_all() to leave only number of comments
+comments <- rstats_html %>% 
+  html_elements(css='.first') %>% 
+  html_text() %>% 
+  str_remove_all(pattern="[A-Za-z]*") %>% 
+  as.numeric() %>% 
+  replace_na(0)
+
+#creating tibble of scraped and cleaned vectors
+rstats_tbl <- tibble(
+    post=post, #do we want the body text or the titles? some posts don't have inner text, taking title for now
+    upvotes=upvotes,
+    comments=comments
+  ) 
+
+
+#Visualization
+
+#ggplot and geom_point plot scatterplot where x=number of upvotes and y=number of comments
+rstats_tbl %>% 
+  ggplot(aes(upvotes,comments)) +
+  geom_point()
+
+#Analysis
+
+#cor.test() runs significance test on correlation between upvotes and comments
+scraped_cortest <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+#0.7807844     
+
+#Publication
+paste0("The correlation between upvotes and comments was r", "(", scraped_cortest$parameter[[1]] ,") = ",
+       round(scraped_cortest$estimate,2), ", p = ",  round(scraped_cortest$p.value,2))
+       
