8f6e2278ade44cf1895dd5107a618a2d95a1b698
Saron M Demeke <demek004@umn.edu>
2023-03-26 16:34:34 -0500

    using download.file for reducing fetch requests and reproducibility. edited publication code. final commit

--- R/reddit_API.R
+++ R/reddit_API.R
@@ -6,14 +6,16 @@ library(tidyverse)
 
 ##Data Import and Cleaning
 
+#download.file() saves local txt file of API fetch for viewing inside R
+download.file("https://www.reddit.com/r/rstats/.json","../data/rawredditjson.txt")
 #fromJSON parson raw information as nested list objects, flatten argument collapses nested data frames
-rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE) 
+rstats_list <- fromJSON("../data/rawredditjson.txt", flatten = TRUE) 
 #the df we want is stored in [["children"]] list element of rstats_list so we extract it
 rstats_original_tbl <- rstats_list$data$children
-#next we create new df with just the desired variables and rename the original JSON names which were collapsed i.e., data.selftext
-rstats_api_tbl <- rstats_original_tbl %>% 
+#following create new df with desired variables and renames the original variable names which were collapsed in nested lists i.e., data.selftext
+rstats_tbl <- rstats_original_tbl %>% 
    select(
-    post=data.title, #do we want the body text or the titles? some posts don't have inner text, taking title for now
+    post=data.title, 
     upvotes=data.ups,
     comments=data.num_comments
   ) 
@@ -21,29 +23,37 @@ rstats_api_tbl <- rstats_original_tbl %>%
 ##Visualization
 
 #ggplot and geom_point plot scatterplot where x=number of upvotes and y=number of comments
-rstats_api_tbl %>% 
+rstats_tbl %>% 
   ggplot(aes(upvotes,comments)) +
   geom_point()
 
 ##Analysis
 
 #cor.test() runs significance test on correlation between upvotes and comments and saves object for use in paste0()
-api_cortest <- cor.test(rstats_api_tbl$upvotes, rstats_api_tbl$comments)
-
+api_cortest <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+#display correlaiton and p-value to console
+api_cortest$estimate
+api_cortest$p.value
 
 
 ##Publication 
-#Console Output: "The correlation between upvotes and comments was r(23) = .43, p = .03. This test was statistically significant."
+#Console Output (03/26 4:08p): "The correlation between upvotes and comments was r(23) = .36, p = .08. This test was not statistically significant."
 
-#paste0 concantenates predefined strings and dynamic portions. str_remove with beginning 0 removes leading zeros
-#round function rounds decimals to 2 digits after decimal point
-#ifelse interprets p <0.05 depending on current p value
+#custom function that takes in a numeric value and specifies the publication format
+#str_remove with regex pattern of starting 0 removes leading zeros
+#round function rounds decimals to 2 digits and format with nsmall argument makes sure at least 2 digits are shown (e.g., in cases where 0.100 rounds to 0.1)
+custom_decimal <-  function(x){
+  str_remove(format(round(x, digits=2L), nsmall=2L), pattern="^0+")
+}
+
+#paste0 concantenates predefined strings and dynamic portions for publication
+#ifelse interprets p < 0.05 depending on current p value
 paste0("The correlation between upvotes and comments was r(",
        api_cortest$parameter[[1]] ,
        ") = ",
-       str_remove(round(api_cortest$estimate,2), pattern="^0+"),
+       custom_decimal(api_cortest$estimate),
        ", p = ",  
-       str_remove(round(api_cortest$p.value,2), pattern="^0+"),
+       custom_decimal(api_cortest$p.value),
        ". This test was",
        ifelse(api_cortest$p.value < .05, " statistically significant.", " not statistically significant.")
 )
--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -2,11 +2,14 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(rvest)
 library(tidyverse)
-library(xml2)
+#library(xml2)
 
 ##Data Import and Cleaning
 
-rstats_html <- read_html("https://old.reddit.com/r/rstats/")
+#download.file() saves local file of the data to scrape to reduce fetch requests while testing code and avoid rate limiting status code
+download.file("https://old.reddit.com/r/rstats/","../data/rawreddithtml.html")
+#read_html reads in saved html file
+rstats_html <- read_html("../data/rawreddithtml.html")
 #using SelectorGadget, I identified the CSS selector to select all post titles (excluded some unnecessary text underneath some post titles)
 post <- rstats_html %>% 
   html_elements(css='.title.may-blank') %>% 
@@ -21,7 +24,7 @@ upvotes <- rstats_html %>%
   replace_na(0)
 #used SelectorGadget to identify selector for every 'X comments' on page
 #before coercing to numeric, I remove all characters using str_remove_all() to leave only number of comments
-comments <- rstats_html1 %>% 
+comments <- rstats_html %>% 
   html_elements(css='.first') %>% 
   html_text() %>% 
   str_remove_all(pattern="[A-Za-z]*") %>% 
@@ -43,24 +46,33 @@ rstats_tbl %>%
   ggplot(aes(upvotes,comments)) +
   geom_point()
 
+
 ##Analysis
 
 #cor.test() runs significance test on correlation between upvotes and comments and saves object for use in paste0()
 scraped_cortest <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
-
+#display correlaiton and p-value to console
+scraped_cortest$estimate
+scraped_cortest$p.value
 
 ##Publication 
-#Console Output: "The correlation between upvotes and comments was r(23) = .43, p = .03. This test was statistically significant."
+#Console Output (03/26 4:08p): "The correlation between upvotes and comments was r(23) = .34, p = .10. This test was not statistically significant."
 
-#paste0 concantenates predefined strings and dynamic portions. str_remove with beginning 0 removes leading zeros
-#round function rounds decimals to 2 digits after decimal point
-#ifelse interprets p <0.05 depending on current p value
+#custom function that takes in a numeric value and specifies the publication format
+#str_remove with regex pattern of starting 0 removes leading zeros
+#round function rounds decimals to 2 digits and format with nsmall argument makes sure at least 2 digits are shown (e.g., in cases where 0.100 rounds to 0.1)
+custom_decimal <-  function(x){
+  str_remove(format(round(x, digits=2L), nsmall=2L), pattern="^0+")
+}
+
+#paste0 concantenates predefined strings and dynamic portions for publication
+#ifelse interprets p < 0.05 depending on current p value
 paste0("The correlation between upvotes and comments was r(",
        scraped_cortest$parameter[[1]] ,
        ") = ",
-       str_remove(round(scraped_cortest$estimate,2), pattern="^0+"),
+       custom_decimal(scraped_cortest$estimate),
        ", p = ",  
-       str_remove(round(scraped_cortest$p.value,2), pattern="^0+"),
+       custom_decimal(scraped_cortest$p.value),
        ". This test was",
        ifelse(scraped_cortest$p.value < .05, " statistically significant.", " not statistically significant.")
 )
